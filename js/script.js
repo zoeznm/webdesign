acddaa9035337c71ff12c347a3604ce79b924cfe
zoeznm <bomikim046@gmail.com>
2024-06-09 16:01:25 +0900

    완성

--- js/script.js
+++ js/script.js
@@ -8,5 +8,28 @@ $(document).ready(function() {
     $(".submenu").stop().slideUp(300);
   });
 
-  // ! 이미지 슬라이드 구현 
-})
+  // ! 이미지 슬라이드 구현 완
+  $(".slide > a:gt(0)").hide();
+
+  setInterval(function(){
+    $('.slide a:first-child').fadeOut()
+    .next('a').fadeIn()
+    .end().appendTo('.slide');
+  }, 3000);
+
+  // ! 탭 메뉴 구현 완
+  $(function(){
+    $('.tabMenu>li>a').click(function(){
+        $(this).parent().addClass("active").siblings().removeClass("active");
+          return false;
+      });
+    });
+  
+  // ! 팝업창 구현 
+  $('.notice li:first').click(function(){
+    $('#modalWrap').addClass("active");
+  });
+  $('.btn').click(function(){
+    $('#modalWrap').removeClass("active");
+  });
+});
